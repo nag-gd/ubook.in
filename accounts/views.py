b8eaa48638414e5586728e1bc930937c06250d18
Nagarjuna <56259048+nag-gd@users.noreply.github.com>
2020-10-09 14:21:31 +0530

    absolute importing

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import (authenticate, get_user_model, login, logout)
-from .forms import UserLoginForm, UserRegisterForm
+from accounts.forms import UserLoginForm, UserRegisterForm
 
  
 
--- home/urls.py
+++ home/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, re_path
-from . import views
+from home import views
 import re
 
 urlpatterns = [
